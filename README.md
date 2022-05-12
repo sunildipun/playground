a32ba7352fe7f4c4f69be98427f7b42b10e3d008
Sunil Behera <sunil.6318ie@gmail.com>
2022-05-12 23:10:14 +0530

    Inial Commit learing React DOM

---
+++ README.md
@@ -0,0 +1,1 @@
+Hey **LEARNING** repo
---
+++ react-beginner/script.js
@@ -0,0 +1,8 @@
+console.log('Will render react');
+//Task 1 Render normal text using h1 or p tag, using reactDOM
+// ReactDOM.render(<p>Hello My God</p>, document.getElementById('root'));
+
+// Task 2 render <ul> with 2+ list in same root id
+ReactDOM.render(<ul><li>Hello</li><li>Hello 2 Hi</li></ul>, document.getElementById('root'));
+
+
