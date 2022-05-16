73a7bf9e0ebeb8d43c79c369af015c6f4744f526
Sunil Behera <sunil.6318ie@gmail.com>
2022-05-16 22:56:18 +0530

    React learning day 3

--- react-beginner/script.js
+++ react-beginner/script.js
@@ -3,6 +3,26 @@ console.log('Will render react');
 // ReactDOM.render(<p>Hello My God</p>, document.getElementById('root'));
 
 // Task 2 render <ul> with 2+ list in same root id
-ReactDOM.render(<ul><li>Hello</li><li>Hello 2 Hi</li></ul>, document.getElementById('root'));
+// ReactDOM.render(<ul><li>Hello</li><li>Hello 2 Hi</li></ul>, document.getElementById('root'));
 
 
+// Task 3 render functional component (Making code composable and resuable)
+// function MainContent() {
+//     return (
+//         <div>
+//             <h1>Main Content Inside body!</h1>
+//         </div>
+//     )
+// }
+
+// ReactDOM.render(<div>
+//     <MainContent />
+// </div>, document.getElementById('root'));
+
+
+//Using vanilla js print h1 tag inside root (Imperative way of implementing)
+const rootElement = document.getElementById('root');
+const h1 = document.createElement('h1');
+h1.textContent = 'Hello World from Earth 52';
+h1.className = 'hello'
+rootElement.appendChild(h1)
